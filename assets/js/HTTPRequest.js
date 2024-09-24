31fe17ace410944e5ca77ade1df1ece3f623ca8b
Victoria Tjia <36994634+victoriatjia@users.noreply.github.com>
2024-09-24 14:07:14 +0700

    Update HTTPRequest.js

--- assets/js/HTTPRequest.js
+++ assets/js/HTTPRequest.js
@@ -32,6 +32,10 @@ function getResource(URL, ResourceName, Parameter, ResponseType, AfterFun){
     */
     xhttp.setRequestHeader("Content-type", 'text/' + ResponseType);
 	if (AfterFun == "bpjs"){
+		xhttp.setRequestHeader("Accept", "*/*")
+		xhttp.setRequestHeader("Accept-Encoding", "gzip, deflate, br")
+		xhttp.setRequestHeader("Connection", "keep-alive")
+		
 		xhttp.setRequestHeader("X-cons-id", cons_id)
 		xhttp.setRequestHeader("X-timestamp", timestamp)
 		xhttp.setRequestHeader("X-signature", signature)
